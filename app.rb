1d8200d895ff4e1b11cf8f18e1fbe947d5b364dc
Celeste Morton <creneemorton@gmail.com>
2020-01-10 13:22:00 -0800

    add first project routes and add css file

--- app.rb
+++ app.rb
@@ -9,3 +9,32 @@ also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
+get('/') do
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:projects)
+end
+
+# Project routes below
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+post('/projects') do
+  title = params[:title]
+  id = params[:id]
+  project = Project.new({:title => title, :id => id})
+  project.save()
+  redirect to ('/projects')
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
