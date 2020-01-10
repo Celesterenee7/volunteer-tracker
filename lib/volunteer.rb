f410afc81933c41fb1d7917296bbf6a697929b7f
Celeste Morton <creneemorton@gmail.com>
2020-01-10 11:53:30 -0800

    add self.find method to volunteer class and pass 2nd test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,5 +7,42 @@ class Volunteer
         @project_id = attributes.fetch(:project_id)
     end
 
+    # def self.all
+    #     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    #     volunteers = []
+    #     returned_volunteers.each() do |volunteer|
+    #       name = volunteer.fetch("name")
+    #       id = volunteer.fetch("id").to_i
+    #       project_id = volunteer.fetch("project_id").to_i
+    #       volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+    #     end
+    #     volunteers
+    #   end
+
+    # def save
+    #     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} ) RETURNING id;")
+    #     @id = result.first().fetch("id").to_i
+    # end
+
+    # def ==(volunteer_to_compare)
+    #     if volunteer_to_compare != nil
+    #       (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    #     else
+    #       false
+    #     end
+    #   end
+
+    def self.find(id)
+        volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+        if volunteer
+          name = volunteer.fetch("name")
+          id = volunteer.fetch("id").to_i
+          project_id = volunteer.fetch("project_id").to_i
+          Volunteer.new({:name => name, :id => id, :project_id => project_id})
+        else
+          nil
+        end
+      end
+
 
 end
