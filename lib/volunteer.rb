5594fcfbb7d7af6de57a271416ef262e9694be19
Celeste Morton <creneemorton@gmail.com>
2020-01-10 12:02:42 -0800

    add self.all method and pass 4th test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,22 +7,22 @@ class Volunteer
         @project_id = attributes.fetch(:project_id)
     end
 
-    # def self.all
-    #     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
-    #     volunteers = []
-    #     returned_volunteers.each() do |volunteer|
-    #       name = volunteer.fetch("name")
-    #       id = volunteer.fetch("id").to_i
-    #       project_id = volunteer.fetch("project_id").to_i
-    #       volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
-    #     end
-    #     volunteers
-    #   end
+    def self.all
+        returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+        volunteers = []
+        returned_volunteers.each() do |volunteer|
+          name = volunteer.fetch("name")
+          id = volunteer.fetch("id").to_i
+          project_id = volunteer.fetch("project_id").to_i
+          volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+        end
+        volunteers
+      end
 
-    # def save
-    #     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} ) RETURNING id;")
-    #     @id = result.first().fetch("id").to_i
-    # end
+    def save
+        result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} ) RETURNING id;")
+        @id = result.first().fetch("id").to_i
+    end
 
     def ==(volunteer_to_compare)
         if volunteer_to_compare != nil
