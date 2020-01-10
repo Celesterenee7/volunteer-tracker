3cdc7ebcb5fa04e4523ae842434ba9b347f4339f
Celeste Morton <creneemorton@gmail.com>
2020-01-10 15:58:49 -0800

    add the option to delete a project online

--- app.rb
+++ app.rb
@@ -37,3 +37,19 @@ get('/projects/:id/add') do
     @project = Project.find(params[:id])
     erb(:new_volunteer)
 end
+
+post('/projects/:id/edit') do
+    name = params[:name]
+    project_id = params[:id]
+    @volunteer = Volunteer.new({:name => name, :id => nil, :project_id => project_id})
+    @volunteer.save()
+    erb(:edit_project)
+end
+
+delete('/projects/:id/edit') do
+    @project = Project.find(params[:id].to_i())
+    @project.delete()
+    @projects = Project.all
+    erb(:show_projects)
+end
+
