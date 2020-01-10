596dfe8be3270217229a3305867ac6d6b1713217
Celeste Morton <creneemorton@gmail.com>
2020-01-10 09:47:41 -0800

    add compare and find id methods, pass third and fourth tests

--- lib/project.rb
+++ lib/project.rb
@@ -7,8 +7,19 @@ class Project
     end
 
     def save
-        result = DB.exec("INSERT INTO cities (title) VALUES ('#{@title}') RETURNING id;")
+        result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
         @id = result.first().fetch("id").to_i
+    end
+
+    def ==(project_to_compare)
+          self.title() == project_to_compare.title()
+    end
+
+    def self.find(id)
+        project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+        title = project.fetch("title")
+        id = project.fetch("id").to_i
+        Project.new({:title => title, :id => id})
     end
 
 end
