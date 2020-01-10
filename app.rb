2a91de94d764503cdeb83b03e0db7a031a0bb60a
Celeste Morton <creneemorton@gmail.com>
2020-01-10 12:59:10 -0800

    add all the FE files to views

---
+++ app.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require ('./lib/volunteer')
+require('pry')
+require('pg')
+also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
