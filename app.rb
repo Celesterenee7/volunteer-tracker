d7894890b9b97da3e84e738fded1b6aaccdfd42d
Celeste Morton <creneemorton@gmail.com>
2020-01-20 14:32:38 -0800

    ensure that all integration specs are now passing

--- app.rb
+++ app.rb
@@ -10,23 +10,15 @@ also_reload('lib/**/*.rb')
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/') do
-  
+  @projects = Project.all
   erb(:home)
 end
 
-# Project routes below
+# ------------ Project routes below ------------- #
 
 get('/projects') do
   @projects = Project.all
-  erb(:show_projects)
-end
-
-post('/projects') do
-  title = params[:title]
-  project = Project.new({:title => title, :id => nil})
-  project.save()
-  @projects = Project.all
-  erb(:show_projects)
+  erb(:show_project)
 end
 
 get('/projects/:id/edit') do
@@ -36,13 +28,27 @@ end
 
 get('/projects/:id/add') do
     @project = Project.find(params[:id])
-    erb(:new_volunteer)
+    erb(:add_volunteer)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id])
+  erb(:show_project)
+end
+
+post('/') do
+  title = params[:title]
+  @project = Project.new({:title => title, :id => nil})
+  @project.save
+  @projects = Project.all
+  erb(:home)
 end
 
 post('/projects/:id/edit') do
     name = params[:name]
     project_id = params[:id]
     @volunteer = Volunteer.new({:name => name, :id => nil, :project_id => project_id})
+    @project = Project.find(params[:id])
     @volunteer.save()
     erb(:edit_project)
 end
@@ -51,8 +57,42 @@ delete('/projects/:id/edit') do
     @project = Project.find(params[:id].to_i())
     @project.delete()
     @projects = Project.all
-    erb(:show_projects)
+    erb(:home)
 end
 
-# Volunteer routes below
+patch('/projects/:id/edit') do
+  @project = Project.find(params[:id])
+  title = params[:title]
+  @project.update({:title => title})
+  @projects = Project.all
+  erb(:home)
+end
 
+# -------- Volunteer routes below ---------- #
+
+get('/volunteers') do
+  @volunteers = Volunteer.all
+  erb(:view_volunteer)
+end
+
+get('/volunteers/:id/edit') do
+  @volunteer = Volunteer.find(params[:id])
+  erb(:edit_volunteer)
+end
+
+delete('/volunteers/:id/edit') do
+  @volunteer = Volunteer.find(params[:id])
+  @volunteer.delete()
+  @volunteers = Volunteer.all
+  @project = Project.find(@volunteer.project_id)
+  erb(:show_project)
+end
+
+patch('/volunteers/:id/edit') do
+  @volunteer = Volunteer.find(params[:id])
+  name = params[:name]
+  @volunteer.update({:name => name})
+  @volunteers = Volunteer.all
+  @project = Project.find(@volunteer.project_id)
+  erb(:show_project)
+end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -10,19 +10,19 @@ class Volunteer
     def self.all
         returned_volunteers = DB.exec("SELECT * FROM volunteers;")
         volunteers = []
-        returned_volunteers.each() do |volunteer|
-        name = volunteer.fetch("name")
-        id = volunteer.fetch("id").to_i
-        project_id = volunteer.fetch("project_id").to_i
-        volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
-      end
+        returned_volunteers.each do |volunteer|
+          name = volunteer.fetch("name")
+          id = volunteer.fetch("id").to_i
+          project_id = volunteer.fetch("project_id").to_i
+          volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+        end
         volunteers
-    end
+      end
 
-    def save
-        result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} ) RETURNING id;")
+      def save
+        result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
         @id = result.first().fetch("id").to_i
-    end
+      end
 
     def ==(volunteer_to_compare)
         if volunteer_to_compare != nil
@@ -50,10 +50,8 @@ class Volunteer
 
     def update(attributes)
         @name = attributes.fetch(:name)
-        @project_id = attributes.fetch(:project_id)
-        DB.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id};")
-        DB.exec("UPDATE volunteers SET project_id = '#{@project_id}' WHERE id = #{@id};")
-    end
+        DB.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id}")
+      end
 
     def delete
         DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
